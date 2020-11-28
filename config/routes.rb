8b3cccf947d82cb9bb3d5c0837d328febc0b1ca2
Tay Yang Wong <wongtakkyan@gmail.com>
2020-11-28 16:38:30 +0100

    success page in progress

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   resources :properties, only: [:new, :create] do
     resources :bookings, only: [:new, :create]
   end
   get "/dashboard", to: 'pages#dashboard'
+  get "/success", to: 'pages#success'
 end
