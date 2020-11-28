ce7d4386487b6acd5f04b3ac05d6e367212ac289
Fabienne Walch <fabienne_walch@hotmail.com>
2020-11-28 17:29:29 +0100

    properties are editable

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/dashboard", to: 'pages#dashboard'
-  resources :properties, only: [:new, :create]
+  resources :properties, only: [:new, :create, :edit, :update]
 
   resources :bookings, only: [] do
     resources :reviews, only: [:new, :create]
