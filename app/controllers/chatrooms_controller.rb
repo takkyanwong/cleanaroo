f19ed6f179b698e90d9fefba4a8e09c15575f435
Martyna Markiel <martyna.markiel@gmail.com>
2020-12-13 11:14:31 +0100

    back end of the chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,14 @@
 class ChatroomsController < ApplicationController
 
+  # get all the bookings from the user
   def index
-    # get all the bookings from the user
-    @chatrooms = Chatroom.where(booking: current_user.bookings)
+    # cleaner:
+    if current_user.role == "cleaner"
+      @chatrooms = Chatroom.where(booking: current_user.bookings)
+    else
+    # prop manager:
+      @chatrooms = Chatroom.where(booking: current_user.property_bookings)
+    end
   end
 
   def show
