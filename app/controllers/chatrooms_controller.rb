b8c4bb925c53907805ca8ba12e8eb198fbdce664
Fabienne Walch <fabienne_walch@hotmail.com>
2020-12-15 21:04:50 +0100

    footer and chat and other buggggs

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,7 +2,11 @@ class ChatroomsController < ApplicationController
 
   def index
     # get all the bookings from the user
-    @chatrooms = Chatroom.where(booking: current_user.bookings)
+    if User.where(role: 1)
+      @chatrooms = Chatroom.where(booking: current_user.bookings)
+    else
+      @chatrooms = Chatroom.where(booking: current_user.booking.property)
+    end
   end
 
   def show
