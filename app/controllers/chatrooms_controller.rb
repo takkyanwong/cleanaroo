d400dc0964e58bf0e9aa99fe996f3f8e1df0e19e
Fabienne Walch <fabienne_walch@hotmail.com>
2020-12-16 21:15:07 +0100

    added button

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -14,6 +14,14 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+
+    # select all messages for chatroom that do not belong to current user
+    # mark these ones as read
+
+    @messages = Message.where(chatroom: @chatroom)
+    @messages.each { |message| message.mark_as_read! for: current_user}
+    #exit part in application controller, because the models in there are called on every page
+    #whenever user leaves the chatroom >> the messages not belonging to the current user are read
   end
 
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,8 @@
 class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   belongs_to :booking
+
+  def count_unread(user)
+    messages.unread_by(user).count
+  end
 end
