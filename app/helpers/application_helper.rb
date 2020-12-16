664a29c883655004eb9f9d2285bbe77b36fa6c60
Martyna Markiel <martyna.markiel@gmail.com>
2020-12-16 21:04:38 +0100

    chat show and index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,29 @@ module ApplicationHelper
       string
     end
   end
+
+  def format_chat_time(date)
+    if date >= Date.today
+       date.strftime("%H:%M")
+    elsif date >= Date.yesterday
+      "Yesterday"
+    elsif (date > Date.today - 7) && (date < Date.yesterday)
+      date.strftime("%A")
+    else
+      date.strftime("%B %-d")
+    end
+  end
+
+  def format_message_time(date)
+    if date >= Date.today
+       "Today at #{date.strftime("%H:%M")}"
+    elsif date >= Date.yesterday
+      "Yesterday at #{date.strftime("%H:%M")}"
+    elsif (date > Date.today - 7) && (date < Date.yesterday)
+      date.strftime("%A at %H:%M")
+    else
+      date.strftime("%B %-d at %H:%M")
+    end
+  end
+
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,21 +3,7 @@ class Chatroom < ApplicationRecord
   belongs_to :booking
 
   def last_message_time
-    last_message = messages.order("created_at asc").last
-    last_message ? last_message.created_at : nil
-    # last_message_time.strftime("%d/%m/%Y")
+    messages.order("created_at asc").last.created_at
   end
-
-  # def custom_format(date)
-  #     if date == Date.today
-  #       "Today"
-  #     elsif date == Date.yesterday
-  #       "Yesterday"
-  #     elsif (created_at > Date.today - 7) && (created_at < Date.yesterday)
-  #       created_at.strftime("%A")
-  #     else
-  #       date.strftime("%B %-d")
-  #     end
-  # end
 
 end
