16a78964728328f8e705a92feadc71228e8db62d
Jamile Radloff <jamilemarcele@gmail.com>
2020-12-18 10:32:58 +0100

    test v3 ssl and domain

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 require_relative 'boot'
 
 require 'rails/all'
-require "cloudflare_proxy"
-
-config.middleware.use CloudflareProxy
+# require "cloudflare_proxy"
+#
+# config.middleware.use CloudflareProxy
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
