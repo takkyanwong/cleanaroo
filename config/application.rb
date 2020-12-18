9735a869203224009728b9c73dc49163f4dc6b8d
Jamile Radloff <jamilemarcele@gmail.com>
2020-12-18 13:34:22 +0100

    test v8

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'lib/cloudflare_proxy'
+require '../lib/cloudflare_proxy'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
