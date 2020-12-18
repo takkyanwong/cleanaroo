65483f2e7baabf27f02ba459759b5ea9cad2bc91
Jamile Radloff <jamilemarcele@gmail.com>
2020-12-18 12:18:16 +0100

    test v7 ssl domain

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-# require 'lib/cloudflare_proxy'
-# config.middleware.use CloudflareProxy
+require 'lib/cloudflare_proxy'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,6 +17,7 @@ module Cleanaroo
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.middleware.use CloudflareProxy
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
